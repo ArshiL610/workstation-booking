0933aef7f97a3e8d0a7041e0dac69831be76727b
ArshiL <91752244+ArshiL610@users.noreply.github.com>
2023-08-01 12:10:54 +0530

    Delete EmailOtp Folder

--- EmailOtp/src/main/java/com/example/workstationBooking/repository/OtpRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.workstationBooking.repository;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.workstationBooking.model.OtpEntity;
-
-public interface OtpRepository extends JpaRepository<OtpEntity, Long>{
-
-	OtpEntity findByOtp(String otp);
-}
